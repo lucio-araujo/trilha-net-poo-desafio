28e5a1c2c602463bcf199877c6afc6e00a56449b
lucio-araujo <lucio.ribeiroaraujo@outlook.com>
2023-10-15 08:26:04 -0300

    Proposta de Solução

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Início dos testes para o Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "+55 (99) 9999-9999", modelo: "N9", imei: "0000-0000-01", memoria: 256);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Brawl Stars");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Início dos testes para o Smartphone IPhone: ");
+Smartphone iphone = new Iphone(numero: "+55 (99) 9999-9999", modelo: "I9", imei: "0000-0000-01", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Salvar Bateria");
+
+Console.ReadLine();
